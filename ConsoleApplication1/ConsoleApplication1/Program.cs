8aa2f6bec3c372d669109d9fc7acd9500469d345
AndreaFloro <andreafloro@outlook.it>
2016-11-21 20:59:40 +0100

    Module_2 submitted for peer review

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = 8, c = 8;
+            char nextLetter = 'X';
+            char otherLetter = 'O';
+            char cPrintLetter = nextLetter;
+
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+
+                    cPrintLetter = nextLetter;
+                    if (j < (c - 1))
+                    {
+
+                        Console.Write(cPrintLetter);
+                        nextLetter = otherLetter;
+                        otherLetter = cPrintLetter;
+                    }
+                    else
+                    {
+                        Console.WriteLine(cPrintLetter);
+                    }
+                }
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ DEV204x/Module_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Module_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; ++i)
+            {
+                for (int j = 0; j < 8; ++j)
+                {
+                    //Both row and column order are even, then write X
+                    if (i % 2 == 0 && j % 2 == 0)
+                    {
+                        Console.Write("X");
+                    }
+                    //Both row and column order are odd, then write X
+                    else if (i % 2 != 0 && j % 2 != 0)
+                    {
+                        Console.Write("X");
+                    }
+                    //Row is even column odd or vice versa
+                    else
+                    {
+                        Console.Write("O");
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
